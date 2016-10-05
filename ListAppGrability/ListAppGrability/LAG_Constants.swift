cf2cba67c11fdc7deaa469c9dbe7faea4a961688
Carlos Parada <carlos@ids.com.br>
2016-10-05 11:20:59 -0500

    Alamofire 3.5.0

---
+++ ListAppGrability/ListAppGrability/LAG_Constants.swift
@@ -0,0 +1,15 @@
+//
+//  LAG_Constants.swift
+//  ListAppGrability
+//
+//  Created by Carlos Parada on 5/10/16.
+//  Copyright © 2016 carlosparada. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+public struct Constants{
+
+
+}
