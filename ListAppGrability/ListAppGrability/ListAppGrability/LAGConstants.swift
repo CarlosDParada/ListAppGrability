6238e7dfead3a781603600a177bceb66eff666d7
Carlos Parada <carlos@ids.com.br>
2016-10-05 12:20:59 -0500

    Embeddede Framework

---
+++ ListAppGrability/ListAppGrability/ListAppGrability/LAGConstants.swift
@@ -0,0 +1,9 @@
+//
+//  LAGConstants.swift
+//  ListAppGrability
+//
+//  Created by Carlos Parada on 5/10/16.
+//  Copyright © 2016 carlosparada. All rights reserved.
+//
+
+import Foundation
---
+++ ListAppGrability/ListAppGrability/ListAppGrability/LAGWebservice.swift
@@ -0,0 +1,9 @@
+//
+//  LAGWebservice.swift
+//  ListAppGrability
+//
+//  Created by Carlos Parada on 5/10/16.
+//  Copyright © 2016 carlosparada. All rights reserved.
+//
+
+import Foundation
